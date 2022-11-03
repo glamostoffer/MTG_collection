ffef4fcc3b8959bfb1e03360ce610406df391fe7
glamostoffer <glamostoffer@gmail.com>
2022-11-03 10:20:10 +0300

    Working at home page. Added grid adaptability.

--- js/scrolling.js
+++ js/scrolling.js
@@ -1,6 +1,7 @@
 document.addEventListener("scroll", eventHandler);
-console.log(123);
 function eventHandler() {
-  console.log("func")
-  document.getElementById("back_grad_1").style.height = `${window.scrollY / 5.6 + 17}%`;
+  document.getElementById("back_grad_1").style.height = `${window.scrollY / 6.2 + 16}%`;
+  document.getElementById("back_grad_2").style.height = `${86 - window.scrollY / 6.2}%`;
+  document.getElementById("back_grad_2").style.marginRight = `${12 - window.scrollY / 25}%`;
+  document.getElementById("back_grad_2").style.marginTop = `${9 + window.scrollY / 10}%`;
 }
