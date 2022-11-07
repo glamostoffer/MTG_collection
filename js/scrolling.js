556858a1e680683781c3f1eb9556f685bc234c4e
glamostoffer <glamostoffer@gmail.com>
2022-11-08 00:27:02 +0300

    Work in progress. Added login page. Worked at home page. Added footer at home page.

--- js/scrolling.js
+++ js/scrolling.js
@@ -1,7 +1,10 @@
 document.addEventListener("scroll", eventHandler);
 function eventHandler() {
-  document.getElementById("back_grad_1").style.height = `${window.scrollY / 6.2 + 16}%`;
-  document.getElementById("back_grad_2").style.height = `${86 - window.scrollY / 6.2}%`;
-  document.getElementById("back_grad_2").style.marginRight = `${12 - window.scrollY / 25}%`;
-  document.getElementById("back_grad_2").style.marginTop = `${9 + window.scrollY / 10}%`;
+  if (window.scrollY <= 400) {
+    document.getElementById("back_grad_1").style.height = `${window.scrollY / 6.2 + 16}%`;
+    console.log(window.scrollY)
+    document.getElementById("back_grad_2").style.height = `${86 - window.scrollY / 6.2}%`;
+    document.getElementById("back_grad_2").style.marginRight = `${12 - window.scrollY / 25}%`;
+    document.getElementById("back_grad_2").style.marginTop = `${9 + window.scrollY / 10}%`;
+  }
 }
